1e040a6bbce210f5ceb54be75722180261a5a8ce
Attila Csordas <attilacsordas@gmail.com>
2014-01-10 22:24:15 +0000

    cleaning up AminoAcidPositionCounterNormalizedToPeptideLength

--- src/main/java/bioinformatics/hadoop/AminoAcidPositionCounterNormalizedToPeptideLength.java
+++ src/main/java/bioinformatics/hadoop/AminoAcidPositionCounterNormalizedToPeptideLength.java
@@ -74,32 +74,6 @@ public class AminoAcidPositionCounterNormalizedToPeptideLength extends Configure
         }
     }
 
-/*
-	@SuppressWarnings("deprecation")
-	public static void main(String[] args) throws Exception {
-
-		JobConf conf1 = new JobConf(NewPosCounter.class);
-		conf1.setJobName("newposcounter");
-
-		conf1.setOutputKeyClass(Text.class);
-		conf1.setOutputValueClass(IntWritable.class);
-
-		conf1.setMapperClass(MyMap.class);
-		// conf.setCombinerClass(Reduce.class);
-		conf1.setReducerClass(MyReduce.class);
-
-		conf1.setInputFormat(TextInputFormat.class);
-		conf1.setOutputFormat(TextOutputFormat.class);
-
-		conf1.setNumReduceTasks(new Integer(10));
-
-		FileInputFormat.setInputPaths(conf1, new Path(args[0]));
-		FileOutputFormat.setOutputPath(conf1, new Path(args[1]));
-
-		JobClient.runJob(conf1);
-	}
-*/
-
     /**
      * kill a directory and all contents
      *
@@ -210,16 +184,13 @@ public class AminoAcidPositionCounterNormalizedToPeptideLength extends Configure
     }
 
 
-    public static final String HADOOP_MACHINE = "hadoop-master-03.ebi.ac.uk";
-    public static final int HADOOP_PORT = 54310;
-
     private static void usage() {
         System.out.println("usage inputfile1 <inputfile2> <inputfile3> ... outputdirectory");
     }
 
     /**
      * Sample of use
-     * args might be /user/slewis/hadoop/test/books/pg135.txt /user/slewis/hadoop/test/output1
+     * args might be /input /output
      *
      * @param args
      * @throws Exception
