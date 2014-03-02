61a78b3ccc66a980df818c3d3cfe1722b2caf2bd
Attila Csordas <attilacsordas@gmail.com>
2014-03-02 00:40:59 +0000

    more mapreduce counters

--- src/main/java/bioinformatics/hadoop/MapReduceCounters.java
+++ src/main/java/bioinformatics/hadoop/MapReduceCounters.java
@@ -7,5 +7,5 @@ public class MapReduceCounters {
 
     // hadoop counter
 
-    static enum PeptideChecker { STARTS_WITH_DIGIT, STARTS_WITH_LETTER }
+    static enum PeptideChecker { STARTS_WITH_DIGIT, STARTS_WITH_LETTER, CONTAINS_DIGIT, CONTAINS_UPPERCASE_LETTERS_ONLY }
 }
